cf79b75ab9c9b587035f66c2f3587865e470e5e0
Meryem <pinarbasi.hm@gmail.com>
2022-03-06 14:06:52 +0100

    part-19 handling fetch errors

--- simple-blog/src/Home.js
+++ simple-blog/src/Home.js
@@ -5,15 +5,24 @@ const Home = () => {
 
     const [blogs , setBlogs ] = useState(null);
     const [isPending , setIsPending ] = useState(true);
+    const [error , setError] = useState(null)
 
       useEffect( () => {
         setTimeout( () => {
             fetch('http://localhost:8000/blogs')
             .then(res => {
+                if(!res.ok){
+                    throw Error('could not fetch the data for that resource')
+                }
                 return res.json()
             })
             .then(data => {
                 setBlogs(data)
+                setIsPending(false)
+                setError(null)
+            })
+            .catch( err => {
+                setError(err.message);
                 setIsPending(false)
             })
         },1000)
@@ -22,6 +31,7 @@ const Home = () => {
 // wenn blogs = null => false somit wird was rechts steht nicht ausgeführt
     return ( 
         <div className="home">
+            { error && <div>{error}</div> }
             { isPending && <div>Loading...</div>}
            {blogs && <BlogList blogs={blogs} title="All Blogs!"/>} 
         </div>
